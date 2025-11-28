8278a98ba1be665a7a71b8c9756f57cc05c12947
Rico <ferdianrico6@gmail.com>
2025-11-28 16:42:12 +0700

    first commit

---
+++ preload.js
@@ -0,0 +1,10 @@
+/** @format */
+
+const { contextBridge, ipcRenderer } = require("electron/renderer");
+
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke("ping"),
+});
